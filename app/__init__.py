bf22354128e3e514a38fd675d942f9d15fb82eb9
Vladimir <vlad.galindo@gmail.com>
2019-09-22 09:29:55 -0500

    setup(core): setting up the environment

---
+++ app/__init__.py
@@ -0,0 +1,53 @@
+import os
+from flask import Flask, url_for
+from flask_jwt_extended import JWTManager
+from flask_mail import Mail
+from flask_restplus import Api
+from pymongo import MongoClient
+from redis import StrictRedis
+from flask_cors import CORS
+
+print("My FLASK_ENVIRONMENT i {}".format(os.getenv('FLASK_ENVIRONMENT')))
+# App
+app = Flask(__name__)
+app.config.from_object(os.getenv('FLASK_ENVIRONMENT', 'config.Development'))
+CORS(app)
+# Email service
+email = Mail(app)
+
+# MongoDB
+mongo_bd_conn = MongoClient(app.config['MONGO_URI'])
+mongo_bd = mongo_bd_conn[app.config['DB_NAME']]
+
+# Redis
+redis_db = StrictRedis(
+    host=app.config['REDIS_HOST'],
+    port=app.config['REDIS_PORT'],
+    db=app.config['REDIS_DB'],
+    password=app.config['REDIS_PASSWORD'],
+    decode_responses=True
+)
+
+# JWT
+jw_token = JWTManager(app)
+@jw_token.token_in_blacklist_loader
+def check_invalid_token(token):
+    jti = token['jti']
+    registry = redis_db.get(jti)
+    if registry is None:
+        return True
+    return registry == 'true'
+
+
+if os.getenv('FLASK_ENVIRONMENT') == "config.Production":
+    @property
+    def specs_url(self):
+        return url_for(self.endpoint('specs'), _external=True, _scheme='https')
+
+    Api.specs_url = specs_url
+api = Api(app, title='LimeHome Booking App', description='LimeHome App', version=1.0)
+@api.errorhandler(Exception)
+def error_handler(err):
+    error_code = err.code
+    sms = err.__str__
+    return {"error_code": error_code, "sms": sms}, error_code
---
+++ config.py
@@ -0,0 +1,53 @@
+import os
+from datetime import timedelta
+
+
+class Config(object):
+    # App
+    APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+    SECRET_KEY = os.getenv('SECRET_KEY', 'fake-secret')
+    APP_PORT = os.getenv('APP_PORT', 5000)
+    APP_URL_PREFIX = os.getenv('APP_URL_PREFIX', "/api/v1.0")
+
+    # MongoDB cloud service
+    DB_HOST = os.getenv('DB_HOST', 'ds217078.mlab.com')
+    DB_USER = os.getenv('DB_USER', 'limehomeadmin')
+    DB_PASSWORD = os.getenv('DB_PASSWORD', 'fake-secret')
+    DB_PORT = os.getenv('DB_PORT', 17078)
+    DB_NAME = os.getenv('DB_NAME', 'limehomebooking')
+    MONGO_URI = f'''mongodb://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'''
+
+    # Email service
+    MAIL_DEFAULT_SENDER = os.getenv('MAIL_USER', "fake-email@gmail.com")
+    MAIL_USERNAME = os.getenv('MAIL_USER', "fake-email@gmail.com")
+    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD', "fake-secret")
+    MAIL_SERVER = os.getenv('MAIL_SERVER', "smtp.gmail.com")
+    MAIL_PORT = os.getenv('MAIL_PORT', "587")
+    MAIL_USE_TLS = True
+    ACTIVATION_URL = os.getenv('ACTIVATION_URL', 'https://limehome-backend.appspot.com{url_prefix}/users/activate/{id}')
+
+    # JWT
+    JWT_SECRET_KEY = SECRET_KEY
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=6)
+    JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=3)
+    JWT_BLACKLIST_ENABLED = True
+    JWT_BLACKLIST_TOKEN_CHECKS = ['access', 'refresh']
+
+    # Redis cloud service
+    REDIS_HOST = os.getenv('REDIS_HOST', 'redis-15255.c55.eu-central-1-1.ec2.cloud.redislabs.com')
+    REDIS_PORT = os.getenv('REDIS_HOST', 15255)
+    REDIS_DB = os.getenv('REDIS_DB', 0)
+    REDIS_PASSWORD = os.getenv('REDIS_PASSWORD', 'fake-secret')
+    ERROR_INCLUDE_MESSAGE = False
+
+    # BFF
+    HERE_PLACES_URL = os.getenv('HERE_PLACES_URL', 'https://places.cit.api.here.com/places/v1/discover/explore')
+    HERE_APP_ID = os.getenv('HERE_APP_ID', 'fake-secret')
+    HERE_APP_CODE = os.getenv('HERE_APP_CODE', 'fake-secret')
+
+class Development(Config):
+    DEBUG = True
+
+
+class Production(Config):
+    DEBUG = False
